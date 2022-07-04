4dd208718a76a34d7096a503dde7aff2bb7635c6
thaisealves <alves.thaisesilva@gmail.com>
2022-07-04 17:32:12 -0300

    feat: adding 'not found' message to user

--- src/Components/SingInPage.js
+++ src/Components/SingInPage.js
@@ -24,11 +24,12 @@ export default function SignInPage() {
       const resp = await axios.post("https://apimywallet.herokuapp.com/sign-in", body);
       localStorage.setItem("token", resp.data.token);
       localStorage.setItem("name", resp.data.name);
-
+      console.log(resp.data)
       navigate("/transactions");
       setDisable(false);
     } catch (error) {
       console.log(error);
+      alert(error.response.data)
       setDisable(false);
       setButtonCtt("Entrar");
     }
